b89643fe46c9a03a104383fd72c0f9d46b4a8eae
31wizard-ash07 <ashish.singh@mechlintech.com>
2022-03-14 21:22:52 +0530

    css added

--- week1/evaluation/src/Components/Vegeies.jsx
+++ week1/evaluation/src/Components/Vegeies.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import styles from './vegeies.module.css'
 const Vegeies = () => {
     const [tomato,setTomatoes]=useState(0);
     const [potato, setPotatoes] = useState(0);
@@ -7,19 +7,20 @@ const Vegeies = () => {
     const [onion, setOnion] = useState(0);
 
     return (
-    <div>
-            <h1>Tomato{tomato > 0 ? "es" : ""} : {tomato} {tomato <= 1?"kg":"Kgs"}</h1>
-        <button onClick={()=>setTomatoes(tomato+1)}>+</button>
-        <button onClick={() => tomato>0?setTomatoes(tomato - 1):alert("Quantity can not be less than 0")}>-</button>
-            <h1>Potato{potato > 0 ? "es" : ""} : {potato} {potato <= 1 ? "kg" : "Kgs"}</h1>
-        <button onClick={()=>setPotatoes(potato+1)}>+</button>
-        <button onClick={() => potato>0?setPotatoes(potato - 1):alert("Quantity can not be less than 0")}>-</button>
-            <h1>Carrot{carrot > 0 ? "s" : ""} : {carrot}{carrot <= 1 ? "kg" : "Kgs"}</h1>
-        <button onClick={()=>setCarrot(carrot+1)}>+</button>
-        <button onClick={() => carrot>0?setCarrot(carrot - 1):alert("Quantity can not be less than 0")}>-</button>
-            <h1>Onion{onion > 0 ? "s" : ""} : {onion} {onion <= 1 ? "kg" : "Kgs"}</h1>
-        <button onClick={()=>setOnion(onion+1)}>+</button>
-        <button onClick={() => onion>0?setOnion(onion - 1):alert("Quantity can not be less than 0")}>-</button>
+    <div className={styles.Vegeies}>
+            <h1 className={styles.heading}>Vegetables</h1>
+            <h1>Tomato{tomato > 1 ? "es" : ""} : {tomato} {tomato <= 1?"kg":"Kgs"}</h1>
+            <button className={styles.counter_btn} onClick={()=>setTomatoes(tomato+1)}>+</button>
+            <button className={styles.counter_btn} onClick={() => tomato>0?setTomatoes(tomato - 1):alert("Quantity can not be less than 0")}>-</button>
+            <h1>Potato{potato > 1 ? "es" : ""} : {potato} {potato <= 1 ? "kg" : "Kgs"}</h1>
+            <button className={styles.counter_btn} onClick={()=>setPotatoes(potato+1)}>+</button>
+            <button className={styles.counter_btn} onClick={() => potato>0?setPotatoes(potato - 1):alert("Quantity can not be less than 0")}>-</button>
+            <h1>Carrot{carrot > 1 ? "s" : ""} : {carrot} {carrot <= 1 ? "kg" : "Kgs"}</h1>
+            <button className={styles.counter_btn} onClick={()=>setCarrot(carrot+1)}>+</button>
+            <button className={styles.counter_btn} onClick={() => carrot>0?setCarrot(carrot - 1):alert("Quantity can not be less than 0")}>-</button>
+            <h1>Onion{onion > 1 ? "s" : ""} : {onion} {onion <= 1 ? "kg" : "Kgs"}</h1>
+            <button className={styles.counter_btn} onClick={()=>setOnion(onion+1)}>+</button>
+            <button className={styles.counter_btn} onClick={() => onion>0?setOnion(onion - 1):alert("Quantity can not be less than 0")}>-</button>
 
     </div>
   )
--- week1/evaluation/src/Components/Wishlist.jsx
+++ week1/evaluation/src/Components/Wishlist.jsx
@@ -4,21 +4,26 @@ const Wishlist = () => {
     const [value, setValue] = useState("")
     const [wishlist, setWishlist] = useState([]);
     const addWishlist = () => {
-        setWishlist([...wishlist, value]);
-        console.log(value);
-        console.log("wishlist", wishlist);
-        setValue("");
+        if(value!==""){
+            setWishlist([...wishlist, value]);
+            console.log(value);
+            console.log("wishlist", wishlist);
+            setValue("");
+        }
+        else 
+        alert("You should wish something")
     }
     return (
-        <div>
-            <input type="text" className={wishlist.length < 3 ? styles.input : styles.disp_none} value={value} onChange={(e) => setValue(e.currentTarget.value)} placeholder="add item to your wishlist"/>
+        <div className={styles.wishlist}>
+            <h1 className={styles.heading}>WISHLIST</h1>
+            <input type="text" className={wishlist.length < 3 ? styles.input_fld : styles.disp_none} value={value} onChange={(e) => setValue(e.currentTarget.value)} placeholder="add item to your wishlist"/>
             <button onClick={addWishlist} className={wishlist.length<3?styles.btn:styles.disp_none}>Add</button>
             <div className={wishlist.length < 3 ? styles.disp_none : styles.message }>
                 <h1>You cannot add more than 3 items to wishlist</h1>
             </div>
             {
                 wishlist.map((item)=>{
-                    return <h1>{item}</h1>
+                    return <h3 className={styles.wishes}>{item}</h3>
                 })
             }
         </div>
