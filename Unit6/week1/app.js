7f641aa9f2a9b77e42c2a164d43c55bdc263eeb0
wizard3107 <ashishsingh9720@gmail.com>
2022-04-18 21:58:12 +0530

    final submit

--- Unit6/week1/app.js
+++ Unit6/week1/app.js
@@ -4,26 +4,32 @@ const author = require('./author.json')
 const library = require('./library.json')
 const server = express()
 
-function logger(req, res, next) {
+function log_console(req, res, next) {
     console.log(req.url)
     next();
 
 }
+let response = {};
 function permission(req, res, next) {
+
     if (req.url) {
+        response["route"] = req.url;
+        response["permission"] = true;
+        console.log(response)
         next();
     }
 
+
 }
-server.get('/books', logger, (req, res, next) => {
+server.get('/books', log_console, (req, res, next) => {
 
     res.json(books)
 })
-server.get("/author",  [logger, permission ], (req, res, next) => {
-    res.json(author)
+server.get("/author", [permission, log_console], (req, res, next) => {
+    res.send(response)
 })
-server.get("/library", [ logger, permission ], (req, res, next) => {
-    res.json(library)
+server.get("/library", [permission, log_console], (req, res, next) => {
+    res.send(response)
 })
 const PORT = 8000
 server.listen(PORT, () => {
